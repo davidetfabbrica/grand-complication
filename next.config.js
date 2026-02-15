70349b1a7e06dc9a62c3f98974f728a8004fb246
DT <davidet.fabbrica@gmail.com>
2026-02-15 23:35:36 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/grand-complication',
+  assetPrefix: '/grand-complication',
   images: {
     unoptimized: true,
   },
